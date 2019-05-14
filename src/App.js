f34b950bcf61cd06308e5f5c9f80bb93e79408e3
vgumonis <vilius.gumonis@coingate.com>
2019-05-14 20:53:49 +0300

    Adds Material components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,85 @@
 import React, {Component} from 'react';
 import Button from '@material/react-button';
-
+import TextField, {HelperText, Input} from '@material/react-text-field';
+import Radio, {NativeRadioControl} from '@material/react-radio';
+import {Cell, Grid, Row} from '@material/react-layout-grid';
 import './app.scss';
 
+const slider = new MDCSlider(document.querySelector('.mdc-slider'));
+slider.listen('MDCSlider:change', () => console.log(`Value changed to ${slider.value}`));
+
 class App extends Component {
+
+
+  
+
+  state = {name: '', lastName: ''};
+  state = {gender: null};
+
   render() {
     return (
+
+
       <div>
+        <Grid align="middle">
+        <Row>
+          <Cell columns={4} align="middle">
+           <TextField
+          label='Vardas'
+          helperText={<HelperText>Įveskite vardą</HelperText>}
+          onTrailingIconSelect={() => this.setState({name: ''})}
+        ><Input
+        value={this.state.name}
+        onChange={(e) => this.setState({name: e.currentTarget.name})}
+           />
+        </TextField>
+        </Cell>
+        </Row>
+        <Row>
+        <Cell columns={4} align="middle">
+        <TextField
+          label='Pavardė'
+          helperText={<HelperText>Įveskite pavardę</HelperText>}
+        ><Input
+        value={this.state.lastName}
+        onChange={(e) => this.setState({lastName: e.currentTarget.lastName})}
+           />
+        </TextField>
+        </Cell>
+        </Row>
+        <Row>
+        <Cell columns={4} align="middle">
+        <Radio label='Moteris' key='moteris'>
+          <NativeRadioControl
+            value='moteris'
+            id='moteris'
+            onChange={(e) => this.setState({gender: e.target.value})}
+          />
+        </Radio>
+        <Radio label='Vyras' key='vyras'>
+          <NativeRadioControl
+            name='vyras'
+            value='vyras'
+            id='vyras'
+            onChange={(e) => this.setState({gender: e.target.value})}
+          />
+        </Radio>
+
+          </Cell>
+          </Row>
+          <Row>
+          <Cell columns={4} align="middle"> 
         <Button
           raised
           className='button-alternate'
-          onClick={() => console.log('clicked!')}
         >
-          Click Me!
-        </Button>
+          Siųsti
+        </Button> 
+        </Cell>
+        
+        </Row>
+      </Grid>
+
       </div>
     );
   }
